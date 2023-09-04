2d50a90e5dcd8953d465a59cfaf8a5399c7cb3fa
Georgi Kostov <p_e_a@gbg.bg>
2023-09-04 20:18:25 +0300

    After trying to update @wdio/sauce-service to 8.14 I'm getting connection errors:
    ERROR @wdio/runner: Failed to run browser tests with cid 0-0: unknown
    error: Failed to load test page to run tests, make sure your browser can
    access "http://localhost:60925"

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,22 +1,67 @@
+import * as url from 'url';
+import http from 'http';
+import express from 'express';
+
+import retrace from '../retrace/index.js';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+let server = null;
+
 export const config = {
-    // ...
-    runner: 'browser',
-    specs: ['./test/index.js'],
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
-    region: 'eu',
-    services: [
-        ['sauce', {
-            sauceConnect: true,
-            sauceConnectOpts: {
-                // ...
-            }
-        }]
-    ],
-    capabilities: [{
-        browserName: 'chrome',
-        platformName: 'Windows 10',
-        browserVersion: '75'
-    }],
-	logLevel: 'trace',
+  runner: 'browser',
+  specs: ['./test/index.js'],
+  framework: 'mocha',
+  reporters: ['spec'],
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60 * 1000,
+  },
+  user: process.env.SAUCE_USERNAME,
+  key: process.env.SAUCE_ACCESS_KEY,
+  region: 'eu',
+  services: [
+    ['sauce', {
+      sauceConnect: true,
+      sauceConnectOpts: {
+      }
+    }]
+  ],
+  capabilities: [{
+    browserName: 'chrome',
+    platformName: 'Windows 11',
+    browserVersion: 'latest'
+  }],
+  logLevel: 'trace',
+  onPrepare: function (_config, _capabilities) {
+    var app = express();
+    app.get(function(req, res, next) {
+      console.log(req.path);
+      next();
+    });
+    app.use(express.static(__dirname + '/test/fixture'));
+    app.get('/retrace', function(req, res) {
+      // Read the stack from a query parameter
+      var stack = req.query.stack;
+      // ... pass it to retrace
+      retrace.map(stack).then(function(s) {
+        // ... and send back the re-mapped stack trace
+        res.send(s);
+      })
+      .catch(function(err) {
+        res.status(500).send(err);
+      })
+      .finally(function() {
+        res.end();
+      });
+    });
+    server = http.createServer(app);
+    server.listen(8001, () => console.log('server listening on port 8001') );
+  },
+  onComplete: function (_exitCode, _config, _capabilities, _results) {
+    if (server) {
+      console.log('stopping server on port 8001');
+      server.close();
+    }
+  }
 };
